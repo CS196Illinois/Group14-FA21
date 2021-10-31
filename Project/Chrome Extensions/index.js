1ba620ba2ebeeb45362c7f344fd06b34e63fb7bc
pravshot <praveenkalva@icloud.com>
2021-10-30 19:20:37 -0500

    changed logo, and updated amazon jump to walgreens jump and got index.js to work

--- Project/Chrome Extensions/index.js
+++ Project/Chrome Extensions/index.js
@@ -1,19 +1,19 @@
 //function that will make the call to backend and update the view
 async function handleSearch(event) {
-    const productName = document.getElementById("product-name").textContent;
+    const productName = document.getElementById("product-name").value;
     const loading = document.getElementById("loading")
     const errors = document.getElementById("errors");
     loading.textContent = "loading..."
     errors.textContent = "";
     try {
-        const response = await fetch(`https://127.0.0.1:3000/multi/${productName}`);
+        const response = await fetch(`http://127.0.0.1:5000/search/${productName}`);
         const jsonResponse = await response.json();
         loading.textContent = "";
-        document.getElementById("first").textContent = "first link";
-        document.getElementById("second").textContent = "second link";
-        document.getElementById("third").textContent = "third link";
-        document.getElementById("fourth").textContent = "fourth link";
-        document.getElementById("fifth").textContent = "fifth link";
+        document.getElementById("first").textContent = jsonResponse.name['0'];
+        document.getElementById("second").textContent = jsonResponse.name['1'];
+        document.getElementById("third").textContent = jsonResponse.name['2'];
+        document.getElementById("fourth").textContent = jsonResponse.name['4'];
+        document.getElementById("fifth").textContent = jsonResponse.name['7'];
     }
     catch (error) {
         loading.textContent = "";
